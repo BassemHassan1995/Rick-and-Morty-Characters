fb5865a83ffc1c078b0ff68c72eda00c0ca10e2c
Bassem Hassan <bassem.hassan1995@gmail.com>
2025-09-15 18:30:04 +0300

    add new splash screen library

--- app/src/main/java/bassem/task/characters/MainActivity.kt
+++ app/src/main/java/bassem/task/characters/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import bassem.task.characters.ui.navigation.AppNavGraph
 import bassem.task.characters.ui.theme.CharactersApplicationTheme
@@ -17,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
